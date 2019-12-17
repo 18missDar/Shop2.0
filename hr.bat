de8431495cfa4c041a2f67cacb98763d20487ab5
18missDar <miss.dar18@mail.ru>
2019-12-17 23:58:33 +0400

    add to Heroku2

--- hr.bat
+++
@@ -1,2 +0,0 @@
-call mvn -B -s settings.xml -DskipTests=true clean install
-call java -Dspring.profiles.active="datajpa,heroku" -DDATABASE_URL="postgres://postgres:password@localhost:5432/topjava" -jar target/dependency/webapp-runner.jar --port $PORT target/*.war
